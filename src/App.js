e42b0a6069885d846542c6832695b0cdb314be67
twhelik <heliktribuwan995@gmail.com>
2023-06-14 11:32:36 +0400

    deploy to heroku server

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          <code>Ronzac App</code> 
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
